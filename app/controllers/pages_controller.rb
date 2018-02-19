bc17364aa935239a45b880cb1e393d4363f2538c
itsme.Rohan <jhambrohan44@gmail.com>
2018-02-19 16:59:44 +0530

    added dashboard to the app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!
   def home
+  	if user_signed_in?
+  		redirect_to dashboard_path
+  	end
   end
+
+  def dashboard
+
+  end
+
 end
